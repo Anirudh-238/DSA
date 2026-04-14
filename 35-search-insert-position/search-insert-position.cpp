3385fa39f3829a783a1b562f1b61c91b6efcf80d
Anirudh Dadwal <anirud238@gmail.com>
2026-04-14 11:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (76.54%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Solution {
+public:
+    int searchInsert(std::vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        // If target is not found, 'low' is the insertion position
+        return low;
+    }
+};
