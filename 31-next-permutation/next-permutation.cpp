ab89d0fb2e04541c8032898d073d154b18b83a85
Anirudh Dadwal <anirud238@gmail.com>
2026-04-14 07:43:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (33.06%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        // Step 1: Find the first decreasing element from the right
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            // Step 2: Find the element just larger than nums[i] to swap with
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            // Step 3: Swap them
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 4: Reverse the suffix to get the smallest lexicographical order
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
